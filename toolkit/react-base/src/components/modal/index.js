4be1a4a5fb459df69ef630e7cedeeec8aa2bf74a
John Cokos <highlander.44@gmail.com>
2017-12-16 06:52:18 +0000

    Updated Toolkit -- redux added

--- toolkit/react-base/src/components/modal/index.js
+++ toolkit/react-base/src/components/modal/index.js
@@ -3,21 +3,26 @@ import './modal.scss';
 import React from 'react';
 
 class Modal extends React.Component {
-    
+
     render() {
-        
+
         return (
-            
+
             <div className="overlay">
                 <div className="modal">
-                    {this.props.children}
-                    <button onClick={this.props.close}>Close Me</button>
+                    <div className="header">
+                        <span class="title">{this.props.title}</span>
+                        <a onClick={this.props.close}>X</a>
+                    </div>
+                    <div className="content">
+                        {this.props.children}
+                    </div>
                 </div>
             </div>
         )
-            
+
     }
-    
+
 }
 
 export default Modal;
